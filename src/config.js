2f4506bcd23b1c49044869fbbfc32d7f2de541ad
ivanpayan <ivanpayanm@gmail.com>
2021-02-09 11:36:37 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const API_ROOT = 'https://api.altcoinomy.com/api/v1';
 export const APP_NAME = 'StakeHound';
-export const FILTER_KEYWORD = 'Stake';
+export const FILTER_KEYWORD = '';
 export const CONFERENCE_HOST_NAME = 'https://onboarding.altcoinomy.com';
