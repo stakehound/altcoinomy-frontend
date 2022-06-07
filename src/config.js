805d23ba9ffa64ccec51e62387494a0a6fd4d812
Edgars <edgars.nemse@gmail.com>
2022-06-07 16:13:57 +0300

    fix: subscription list

--- src/config.js
+++ src/config.js
@@ -1,8 +1,15 @@
-export const API_ROOT = 'https://altcoinomy.com/api/v1';
+export const API_ROOT = 'https://api.altcoinomy.com/api/v1';
 export const APP_NAME = 'StakeHound';
-export const FILTER_KEYWORD = 'Stakehound';
-export const EXCLUDE = 'StakeHound stETH';
+export const FILTER_KEYWORD = 'StakeHound';
+export const EXCLUDE = '';
 export const CONFERENCE_HOST_NAME = 'https://onboarding.altcoinomy.com';
 
 
-export const PROJECTS_TO_DISPLAY = null;
+export const PROJECTS_TO_DISPLAY =  null
+// export const PROJECTS_TO_DISPLAY = [
+//     '629f4da0a4b7ee064623fba2', // stDash
+//     '629f4dbba9521d25c37228a9', // stFiro
+//     '629f4e15a4b7ee064623fbab', // stXem
+//     '629f4e2fa4b7ee064623fbb0', // stXym
+//     '629f4e49a9521d25c37228b3', // stZen
+// ];
